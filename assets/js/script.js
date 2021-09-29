d0ee27881c0cfb76f32951ac14379fff8c766021
Caitlin Wall <caitlinwall96@gmail.com>
2021-09-29 13:05:36 -0400

    Add functioning make teams button

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,13 +5,21 @@ function getRandomNum(min, max) {
 
 //Make two randomized teams
 function makeTeams() {
-    var main = document.getElementById("main");
+    var yourTeam = document.getElementById("yourTeam");
+    var computerTeam = document.getElementById("computerTeam");
     var input = document.getElementById("numPokemon").value;
     var pokeArray = [];
+    var opponentArray = [];
     while(pokeArray.length <= input - 1){
         var randomPokemon = getRandomNum(0, 151);
         if(pokeArray.indexOf(randomPokemon) === -1) {
             pokeArray.push(randomPokemon);
+        };
+    }
+    while(opponentArray.length <= input - 1){
+        var randomPokemon = getRandomNum(0, 151);
+        if(opponentArray.indexOf(randomPokemon) === -1) {
+            opponentArray.push(randomPokemon);
         };
     }
     for(i=0; i<input; i++) {
@@ -20,21 +28,41 @@ function makeTeams() {
         var pokeUrl = "https://pokeapi.co/api/v2/pokemon/" + pokeArrayInput;
         fetch(pokeUrl)
         .then(function (response) {
-            console.log(response);
             return response.json();
         })
         .then(function (data) { 
-            
-        
             var container = document.createElement("div");
             var pokePic = document.createElement("img");
             pokePic.setAttribute("src", data.sprites.front_default);
             var pokeName = document.createElement ("p");
             pokeName.textContent = data.name;
+            container.className = "inline-block";
+            
+                    
+            yourTeam.appendChild(container);
+            container.appendChild(pokePic);
+            container.appendChild(pokeName);
+        })
+    }
+    for(i=0; i<input; i++) {
+        
+        var opponentArrayInput = opponentArray[i];
+        var opponentUrl = "https://pokeapi.co/api/v2/pokemon/" + opponentArrayInput;
+        fetch(opponentUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) { 
+            var container = document.createElement("div");
+            var pokePic = document.createElement("img");
+            pokePic.setAttribute("src", data.sprites.front_default);
+            var pokeName = document.createElement ("p");
+            pokeName.textContent = data.name;
+            container.className = "inline-block";
            
             
                     
-            main.appendChild(container);
+            computerTeam.appendChild(container);
             container.appendChild(pokePic);
             container.appendChild(pokeName);
         })
